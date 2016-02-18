e495957b844cf88ab231ea1050540ae5ddcf2888
Soo <sootracker@gmail.com>
2016-02-18 20:51:21 +0800

    add SPUtils.java

--- src/main/java/soo/swallow/base/SPUtils.java
+++ src/main/java/soo/swallow/base/SPUtils.java
@@ -18,28 +18,38 @@ package soo.swallow.base;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.text.TextUtils;
 
 import java.io.File;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
+import java.util.Map;
+import java.util.WeakHashMap;
 
 import soo.swallow.base.exception.ReasonException;
 
 /**The SharedPreferences tools kit
  * Created by Soo.
  */
-public class SPUtils {
+public final class SPUtils {
+
+    private static Map<String, SharedPreferences> spReceiver = new WeakHashMap<String, SharedPreferences>();
 
     /**Get instance of SharedPreferences by particular directory and name.
      * @param context getSharedPreferences by context when the directory file is null
      * @param directoryFile The directory which is the parent of xml file
      * @param name The name of xml file
-     * @param mod The access of xml file for read or write
+     * @param mod The permission of xml file for read or write
      * @return The instance of SharedPreferences
      * @throws ReasonException
+     * @throws IllegalArgumentException
      */
     public static SharedPreferences getSharedPreferences(Context context, File directoryFile, String name, int mod) throws ReasonException {
-        if (directoryFile == null) {
+        if (TextUtils.isEmpty(name)) {
+            throw new IllegalArgumentException("Name must is not empty");
+        }
+        File tempFile = context.getDir("shared_prefs", Context.MODE_PRIVATE);
+        if (directoryFile == null || directoryFile.equals(tempFile)) {
             return context.getSharedPreferences(name, mod);
         }
         if (!directoryFile.isDirectory()) {
@@ -49,27 +59,35 @@ public class SPUtils {
             name += ".xml";
         }
         File spFile = FileUtils.createFile(directoryFile, name, false);
+
         if (spFile == null || !spFile.exists()) {
             throw new RuntimeException("Can not create target file[" + name + "]which parent is[" + directoryFile + "]");
         }
-        try {
-            Class<?> implCls = Class.forName("android.app.SharedPreferencesImpl");
-            Constructor<?> cons = implCls.getDeclaredConstructor(File.class, int.class);
-            cons.setAccessible(true);
-            Object obj = cons.newInstance(spFile, mod);
-            return (SharedPreferences) obj;
-        } catch (ClassNotFoundException e) {
-            throw new ReasonException("Failed, because:" + e.getMessage());
-        } catch (NoSuchMethodException e) {
-            throw new ReasonException("Failed, because:" + e.getMessage());
-        } catch (InstantiationException e) {
-            throw new ReasonException("Failed, because:" + e.getMessage());
-        } catch (IllegalAccessException e) {
-            throw new ReasonException("Failed, because:" + e.getMessage());
-        } catch (IllegalArgumentException e) {
-            throw new ReasonException("Failed, because:" + e.getMessage());
-        } catch (InvocationTargetException e) {
-            throw new ReasonException("Failed, because:" + e.getMessage());
+        synchronized (spReceiver) {
+            String spKey = spFile.getAbsolutePath();
+            SharedPreferences instance = spReceiver.get(spKey);
+            if (instance == null) {
+                try {
+                    Class<?> implCls = Class.forName("android.app.SharedPreferencesImpl");
+                    Constructor<?> cons = implCls.getDeclaredConstructor(File.class, int.class);
+                    cons.setAccessible(true);
+                    instance = (SharedPreferences)cons.newInstance(spFile, mod);
+                    spReceiver.put(spKey, instance);
+                } catch (ClassNotFoundException e) {
+                    throw new ReasonException("Failed, because:" + e.getMessage());
+                } catch (NoSuchMethodException e) {
+                    throw new ReasonException("Failed, because:" + e.getMessage());
+                } catch (InstantiationException e) {
+                    throw new ReasonException("Failed, because:" + e.getMessage());
+                } catch (IllegalAccessException e) {
+                    throw new ReasonException("Failed, because:" + e.getMessage());
+                } catch (IllegalArgumentException e) {
+                    throw new ReasonException("Failed, because:" + e.getMessage());
+                } catch (InvocationTargetException e) {
+                    throw new ReasonException("Failed, because:" + e.getMessage());
+                }
+            }
+            return instance;
         }
     }
 
